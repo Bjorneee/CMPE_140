f54209fe7a9b0be2352cc009983baab854e5fcd2
Bjorneee <benicimo4@gmail.com>
2025-12-03 03:20:13 -0800

    updated to include rest of vivado labs

---
+++ project_lab7/project_lab7.srcs/sources_1/new/shifter.v
@@ -0,0 +1,10 @@
+module shifter (
+    input  wire [31:0] d,
+    input  wire [4:0]  shamt,
+    input  wire        dir,
+    output wire [31:0] q
+    );
+
+    assign q = (dir) ? (d >> shamt) : (d << shamt);
+    
+endmodule
